4eb6a0834507fb8b5fe185dc10d1dfc5279d2101
Kir_Vasilev <vasilevkirill.97@mail.ru>
2025-11-17 02:04:32 +0300

    add react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import cat from "./cat.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={cat} className="App-logo" alt="logo" />
+        <p>Hello Tender</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
